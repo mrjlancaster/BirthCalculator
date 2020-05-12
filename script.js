d55329cd24ff2380a8415832563218965b0af851
mrjlancaster <lancast001@gmail.com>
2020-05-12 01:56:35 -0500

    changed repository

---
+++ script.js
@@ -0,0 +1,16 @@
+const runCalc = document.getElementById("button");
+const number = document.getElementById("number").value;
+
+runCalc.addEventListener('click', calculate);
+
+function calculate() {
+    const number = document.getElementById("number").value;
+        if (number == "" || number == 0) {
+            alert('Please add your age');
+        }
+
+        let date = new Date().getFullYear();
+        let result = date - number;
+            document.getElementById("year").style.display = "inline-block";
+            document.getElementById("year").innerHTML = result;
+}
